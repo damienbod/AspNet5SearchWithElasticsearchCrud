7ae7612499bd1ad923f7ed0140916028d0efe9e7
damienbod <damien_bod@hotmail.com>
2015-11-17 23:30:20 +0100

    code clean up

--- src/AspNet5SearchWithElasticsearchCrud/Startup.cs
+++ src/AspNet5SearchWithElasticsearchCrud/Startup.cs
@@ -17,35 +17,24 @@ namespace AspNet5SearchWithElasticsearchCrud
         {
         }
 
-        // This method gets called by a runtime.
-        // Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
-            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
-            // services.AddWebApiConventions();
 
             services.AddScoped<ISearchProvider, ElasticSearchProvider>();
         }
 
-        // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.MinimumLevel = LogLevel.Information;
             loggerFactory.AddConsole();
             loggerFactory.AddDebug();
 
-            // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
 
-            // Configure the HTTP request pipeline.
             app.UseStaticFiles();
 
-            // Add MVC to the request pipeline.
             app.UseMvc();
-            // Add the following route for porting Web API 2 controllers.
-            // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
         }
     }
 }
